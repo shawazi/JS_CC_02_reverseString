7ab08d37925fed9046805d91fbf332381cda7d99
shawazi <shawazisonfire@gmail.com>
2022-12-12 14:00:24 -0500

    not sure why it's not working in jsbin

---
+++ main.js
@@ -0,0 +1,11 @@
+let string = prompt("Yeah, hi, what's the string you're reversing? Enter it here: ")
+
+function reverse(string) {
+    let reversed = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
+}
+
+reverse(string);
